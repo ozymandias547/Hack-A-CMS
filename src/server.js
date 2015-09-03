15e6529f195f6b7ff02b461a1033e5623c216696
Joseph Jung <joejung@Administrators-MacBook-Pro-2.local>
2015-09-03 08:56:05 -0500

    Add react renderToString example.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
 // TODO: write CMS server code.
+var React = require("react");
+require("babel/register");  // Allows require('.jsx') files to be compiled on the fly on node.
+
+var HelloComponent = require('./component/test-component.jsx');
+var HelloComponentInstance = React.createElement(HelloComponent, { name: "World" });
+
+console.log(React.renderToString(HelloComponentInstance));  // The rendered template string to be shipped down to the client.
+
